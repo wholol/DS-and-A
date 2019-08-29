45d6286ae98ae576f61402e7838eadc013fd7ef5
Jeremy <jsow1997@gmail.com>
2019-08-29 18:25:33 +1000

    added search operations

--- hashing/linear probing.c
+++ hashing/linear probing.c
@@ -9,22 +9,73 @@ int hashfunc(int key){
     return key%SIZE;
 }
 
+int linearhashfunc(int index){
+    int i = 0;
+     while (hash[(index + i)%SIZE] != 0){
+            i = i + 1;
+        }
+    return (index + i)%SIZE;
+}
 
 
 void insertkey(int key){
     int index = hashfunc(key);      //let index be key%SIZE
 
     if (hash[index] != 0){
-        int i = 0;
-        while (hash[(index + i)%SIZE] != 0){
-            i = i + 1;
-        }
-        hash[(index + i)%SIZE] = key;
+        int newindex = linearhashfunc(index);
+        hash[newindex] = key;
     }
 
     else{
         hash[index] = key;
     }
+
+    if (index == SIZE){
+        printf("%d, no more space");
+    }
+}
+
+void printlist(){
+for (int i = 0 ; i < SIZE; ++i)
+{
+    printf("%d",hash[i]);
+    printf(" ");
+}
+}
+
+void linearhashfuncsearch(int searchindex,int key){        //searching for linear hash func
+    int i = 0;
+    while (hash[(searchindex + i)%SIZE] != key && (searchindex + i)%SIZE != SIZE){
+        i = i + 1;
+        if (hash[(searchindex + i)%SIZE] == key){
+            printf("%d",key);
+            printf(" ");
+            printf("%d", (searchindex + i)%SIZE);
+        }
+
+        else{
+             printf("key not found");
+             break;
+        }
+    }
+
+   
+ 
+}
+
+void search(int key){
+    int searchindex = hashfunc(key);
+    if (hash[searchindex] != key){      //if searchindex is not the key
+        linearhashfuncsearch(searchindex,key);
+    }
+    else{
+        if (hash[searchindex] == key){
+            printf("%d", key);
+            printf("");
+            printf("%d", searchindex);
+        }
+    }
+
 }
 
 int main(){
@@ -33,12 +84,10 @@ insertkey(12);
 insertkey(25);
 insertkey(35);
 insertkey(26);
+printlist();
+search(2);
 
-for (int i = 0 ; i < SIZE; ++i)
-{
-    printf("%d",hash[i]);
-    printf(" ");
-}
+
 
 
 
