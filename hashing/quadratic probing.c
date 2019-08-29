d5203dc2ff11db3ecc9dc7468068c385309240c8
Jeremy <jsow1997@gmail.com>
2019-08-29 18:34:34 +1000

    quad hash

--- hashing/quadratic probing.c
+++ hashing/quadratic probing.c
@@ -9,7 +9,7 @@ int hashfunc(int key){
     return key%SIZE;
 }
 
-int linearhashfunc(int index){
+int quadhashfunc(int index){
     int i = 0;
      while (hash[(index + i*i)%SIZE] != 0){     //only difference from linear probing is this function
             i = i + 1;
@@ -22,7 +22,7 @@ void insertkey(int key){
     int index = hashfunc(key);      //let index be key%SIZE
 
     if (hash[index] != 0){
-        int newindex = linearhashfunc(index);
+        int newindex = quadhashfunc(index);
         hash[newindex] = key;
     }
 
@@ -43,14 +43,14 @@ for (int i = 0 ; i < SIZE; ++i)
 }
 }
 
-void linearhashfuncsearch(int searchindex,int key){        //searching for linear hash func
+void quadhashfuncsearch(int searchindex,int key){        //searching for linear hash func
     int i = 0;
-    while (hash[(searchindex + i)%SIZE] != key && (searchindex + i)%SIZE != SIZE){
+    while (hash[(searchindex + i*i)%SIZE] != key && (searchindex + i*i)%SIZE != SIZE){
         i = i + 1;
-        if (hash[(searchindex + i)%SIZE] == key){
+        if (hash[(searchindex + i*i)%SIZE] == key){
             printf("%d",key);
             printf(" ");
-            printf("%d", (searchindex + i)%SIZE);
+            printf("%d", (searchindex + i*i)%SIZE);
         }
 
         else{
@@ -66,7 +66,7 @@ void linearhashfuncsearch(int searchindex,int key){        //searching for linea
 void search(int key){
     int searchindex = hashfunc(key);
     if (hash[searchindex] != key){      //if searchindex is not the key
-        linearhashfuncsearch(searchindex,key);
+        quadhashfuncsearch(searchindex,key);
     }
     else{
         if (hash[searchindex] == key){
